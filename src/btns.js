dd32d0021d7d159b935ac9900f1028ba95331b38
FranAlc <fgd.galan@gmail.com>
2023-09-16 03:21:08 -0300

    Modifico el codigo js y dejo ejemplos de cod ant

--- src/btns.js
+++ src/btns.js
@@ -8,20 +8,30 @@
         window.open(instagramUrl, '_blank');
     }
 });
-*/
-//Instagram opcion2
-const btn_ig = document.getElementById('btn_perfil_ig');
-function abrirIg(){
-    const usuarioIg = getElementById('igPerfil').value;
-    const instagramUrl =`https://www.instagram.com/${usuarioIg}/`;
-    window.open(instagramUrl, '_blank');
-}
-btn_ig.addEventListener('click', abrirIg());
-
 //Toolzu, se llama a la funcion desde html con onclick
-/*function toolzuSearch(){
+function toolzuSearch(){
     var usuario = document.getElementById('igToolzu').value;
     var toolzuUrl = `https://toolzu.com/profile-analyzer/instagram/?username=${usuario}`;
     window.open(toolzuUrl, '_blank');
-}*/
+}
+*/
+//Instagram
+const btn_ig = document.getElementById('btn_perfil_ig');
+function abrirIg(){
+    const usuarioIg = document.getElementById('igPerfil').value;
+    const instagramUrl =`https://www.instagram.com/${usuarioIg}/`;
+    window.open(instagramUrl, '_blank');
+}
+btn_ig.addEventListener('click', abrirIg);
+
+//Toolzu
+const btn_Toolzu = document.getElementById('btn_perfil_toolzu');
+function abrirToolzu(){
+    const usuarioIg = document.getElementById('igToolzu').value;
+    const toolzuUrl = `https://toolzu.com/profile-analyzer/instagram/?username=${usuarioIg}`;
+    window.open(toolzuUrl, '_blank');
+}
+btn_Toolzu.addEventListener('click', abrirToolzu);
+
+//Google Busqueda
 
