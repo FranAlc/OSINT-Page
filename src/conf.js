e9b0a07e030b9a473af1c33eac45e90b23564198
FranAlc <fgd.galan@gmail.com>
2023-09-17 03:17:32 -0300

    agrego la funcion autocompletar

--- src/conf.js
+++ src/conf.js
@@ -1,5 +1,6 @@
-const autocompletarTodoButton = document.getElementById('autocompletarTodo');
-const autocompletarInput = document.getElementById('autocompletar');
+const autocompletarTodoButton = document.getElementById('btn_autocompletar_ig');
+const autocompletarInput = document.getElementById('autocompletarIg');
+
 
 autocompletarTodoButton.addEventListener('click', function(event) {
     event.preventDefault(); // Evitar el envío del formulario
@@ -12,3 +13,24 @@ autocompletarTodoButton.addEventListener('click', function(event) {
     document.getElementById('busquedaBing').value = valorAutocompletar;
 });
 
+//Tw autocompletar
+function autocompletarTw(event){
+    event.preventDefault();//agrego metodo event y lo utilizo nuevamente.
+
+    const autocompletarTw = document.getElementById('autocompletarTw').value;
+
+    document.getElementById('twPerfil').value = autocompletarTw;
+    document.getElementById('tweets').value = autocompletarTw;
+    document.getElementById('twLike').value = autocompletarTw;
+    document.getElementById('twRespuestas').value = autocompletarTw;
+    document.getElementById('twSeguidores').value = autocompletarTw;
+    document.getElementById('twSeguidos').value = autocompletarTw;
+    document.getElementById('twWebCache').value = autocompletarTw;
+    document.getElementById('twSocialBearing').value = autocompletarTw;
+    document.getElementById('twGoogle').value = autocompletarTw;
+    document.getElementById('twBing').value = autocompletarTw;
+    document.getElementById('twTwitonomy').value = autocompletarTw;
+
+}
+const btn_autocompletar_tw = document.getElementById('btn_autocompletar_tw');
+btn_autocompletar_tw.addEventListener('click', autocompletarTw);
