8e9dd5e0a03bccce44526a04a5750a621103e94d
FranAlc <fgd.galan@gmail.com>
2023-09-22 17:49:59 -0300

    modifique las funciones de los botones y el orden

--- src/js/botones_tools_reddit_tiktok.js
+++ src/js/botones_tools_reddit_tiktok.js
@@ -9,6 +9,15 @@ const btn_buscar_perfil_reddit = document.getElementById('btn_buscar_perfil_redd
 btn_buscar_perfil_reddit.addEventListener('click', buscarUsernameReddit);
 
 //Persona + publicación - Reddit
+function buscarPersonaMasPublicacion(event){
+    event.preventDefault();
+    const inputName = document.getElementById('rdNombreUsuario2').value; //usuario
+    const inputPublicacion = document.getElementById('rdPublicacion').value; //publicacion del usuario ingresado
+    const url = `https://www.reddit.com/r/${inputName}/search/?q=${inputPublicacion}`;
+    window.open(url, '_blank');
+}
+const btn_buscar_personaEspecifica_reddit = document.getElementById('btn_buscar_personaEspecifica_reddit');
+btn_buscar_personaEspecifica_reddit.addEventListener('click', buscarPersonaMasPublicacion);
 
 
 //Buscar en Google - Reddit
@@ -25,9 +34,11 @@ btn_buscar_google_reddit.addEventListener('click', buscarGoogleReddit);
 function buscadorGlobalReddit(event){
     event.preventDefault();
     const inputName = document.getElementById('rdGoogle').value;
-    const url = `${inputName}`; //bing
-    const url2 = `${inputName}`; //yahoo
+    const url = `https://www.bing.com/search?q=site%3A+reddit.com+%27${inputName}%27`; //bing
+    const url2 = `https://yandex.com/search/?text=site%3A+reddit.com+%27${inputName}%27`; //yandex
     window.open(url, '_blank');
+    window.open(url2, '_blank');
+    
 }
 const btn_buscador_global_reddit = document.getElementById('btn_buscador_global_reddit');
 btn_buscador_global_reddit.addEventListener('click', buscadorGlobalReddit);
