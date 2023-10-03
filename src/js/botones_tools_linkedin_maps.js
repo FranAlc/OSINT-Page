7b9d45d9ff6151a0bc74c774bbc591ce975e1a89
Franco Galan <99420835+FranAlc@users.noreply.github.com>
2023-10-02 23:53:01 -0300

    agrego link de imgs y buscar persona

--- src/js/botones_tools_linkedin_maps.js
+++ src/js/botones_tools_linkedin_maps.js
@@ -54,3 +54,24 @@ function abrirBuscadorNombreEvento(event){
 }
 const btn_buscar_nombre_evento = document.getElementById('btn_buscar_nombre_evento');
 btn_buscar_nombre_evento.addEventListener('click',abrirBuscadorNombreEvento);
+
+//Nombre persona
+
+function abrirBuscadorNombrePersona(event){
+    event.preventDefault();
+    const usuario = document.getElementById('lkPerfil').value;
+    const url = `https://www.linkedin.com/in/${usuario}/`;
+    window.open(url, '_blank');
+}
+const btn_buscar_nombre_persona = document.getElementById('btn_buscar_nombre_persona');
+btn_buscar_nombre_persona.addEventListener('click',abrirBuscadorNombrePersona);
+
+//Buscar imgs 
+function abrirBuscadorNombreFoto(event){
+    event.preventDefault();
+    const usuario = document.getElementById('lkFotosGoogle').value;
+    const url = `https://www.google.com/search?q=site:linkedin.com+%27${usuario}%27&sca_esv=570220637&tbm=isch&sxsrf=AM9HkKm7dUqMJp_hzJ25o0DAd67SFs5lYg:1696301397068&source=lnms&sa=X&ved=2ahUKEwiP49CR79iBAxVhq5UCHSm4DY8Q_AUoAXoECAEQAw${usuario}/`;
+    window.open(url, '_blank');
+}
+const btn_buscar_foto_googlelk = document.getElementById('btn_buscar_foto_googlelk');
+btn_buscar_foto_googlelk.addEventListener('click',abrirBuscadorNombreFoto);
